922421f6aeb240adbe48d21a813a2277c857088c
Rick L <richyray24@gmail.com>
2022-06-18 17:48:10 -0400

    added confirmation message before deleting records

--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -6,25 +6,10 @@
     {
         console.log("App Started!");
 
-        // use AJAX to read JSON file
-
-        // Step 1: Create a new XHR object
-        let XHR = new XMLHttpRequest();
-
-        // Step 2: Create the request
-        XHR.open("GET", "../data/data.json");
-
-        // Step 3: Send the request
-        XHR.send();
-
-        // Step 4: create an event listener/handler
-        XHR.addEventListener("readystatechange", function()
-        {
-            if(XHR.readyState == 4 && XHR.status == 200)
-            {
-                console.log("JSON Data:");
-                console.log("===========");
-                console.log(XHR.responseText);
+        $("a.delete").on("click", function(event){
+            if(!confirm("Are you sure?")){
+                event.preventDefault();
+                location.href = "/contact-list";
             }
         });
     }
